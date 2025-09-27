9df1185534c2fd028c310684b4c3582beb306881
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-09-27 10:08:01 +0530

    preparing to deploy to vercel

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -7,14 +7,14 @@ import { useLoading } from "../providers/LoadingProvider";
 
 const page = () => {
   const { signInUser, session, loading: authLoading } = useAuth();
-  
+
   const router = useRouter();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const {setLoading:pageLoading} = useLoading();
+  const { setLoading: pageLoading } = useLoading();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault(); // prevent page refresh
@@ -39,14 +39,13 @@ const page = () => {
       router.push("/Dashboard");
     }
   }, [session, authLoading, router]);
-  
 
   return (
     <div className="w-[90%] h-full flex flex-col items-center !m-auto gap-8 !mt-16">
       <Heading>Signin to your account</Heading>
       <form onSubmit={handleSubmit} className="w-full flex flex-col gap-10">
         <div className="w-full flex flex-col gap-4">
-          <Input            
+          <Input
             type="email"
             name="email"
             value={email}
@@ -54,7 +53,7 @@ const page = () => {
             required
             placeholder="Eg.saptarshi@example.com"
           />
-          <Input            
+          <Input
             type="password"
             name="password"
             value={password}
@@ -67,19 +66,30 @@ const page = () => {
           </Link>
         </div>
         <div className="w-full flex flex-col gap-4">
-          <Button type="submit" loading={loading} variant={"solid"} colorPalette={"teal"}>
+          <Button
+            type="submit"
+            loading={loading}
+            variant={"solid"}
+            colorPalette={"teal"}
+          >
             Sign In
           </Button>
-           <Button  variant={"solid"}>
-          Sign In with Gmail
-        </Button>
-        </div>        
+          <Button variant={"solid"}>Sign In with Gmail</Button>
+        </div>
       </form>
       <div className="flex gap-2 text-gray-400">
-      <p>Don't have an account?</p>
-      <Link color={'teal.300'} _hover={{color:'teal.400'}} onClick={() => {router.push('/SignUp');pageLoading(true)}}>Sign Up</Link> 
+        <p>Don't have an account?</p>
+        <Link
+          color={"teal.300"}
+          _hover={{ color: "teal.400" }}
+          onClick={() => {
+            router.push("/SignUp");
+            pageLoading(true);
+          }}
+        >
+          Sign Up
+        </Link>
       </div>
-           
     </div>
   );
 };
