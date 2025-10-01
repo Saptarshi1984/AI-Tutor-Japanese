a470c2c4bd1219e07253335c80f753bc745de9b6
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-10-01 08:16:33 +0530

    Fixing different mobile devices view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, [session, loading, route]);
 
   return (
-    <main className="relative w-[412px] min-h-[830px] max-h-[930px]">
+    <main className="relative ">
       <HomePage />
            
     </main>
